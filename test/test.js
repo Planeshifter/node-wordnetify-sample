ff6b15997652c9040fbbe6510e137e07df44541b
Planeshifter <pburckhardt@outlook.com>
2015-04-02 02:07:57 -0400

    [UPDATE] multi-category support

---
+++ test/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var annotateRandomSample = require( '../lib/annotateRandomSample');
+var generateRandomSample = require( '../lib/generateRandomSample');
+
+var chai = require( 'chai' );
+var expect = chai.expect;
